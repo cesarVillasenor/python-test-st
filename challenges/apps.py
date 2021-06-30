395b434cec48a6dbf51876a3b7537c88ec311e8e
Cesar <cesar.villasenor.g@gmail.com>
2021-06-29 20:25:19 -0700

    (v0.1) app renamed and .

--- challenges/apps.py
+++ challenges/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ChanllengesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chanllenges'
+    name = 'apps.challenges'
