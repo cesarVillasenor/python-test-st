491a78f0ab5db5b51e4f19949f4bef7dfdd508c4
Cesar <cesar.villasenor.g@gmail.com>
2021-06-29 22:02:38 -0700

    (v0.1) fixing git since it was not int project root.

--- challenges/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import DetectingChange, SeasonProblem, CustomOrderStatus
-
-urlpatterns = [
-    path('detecting-change', DetectingChange.as_view(), name="detecting-change"),
-    path('season-problem', SeasonProblem.as_view(), name="season-problem"),
-    path('customer-order-status', CustomOrderStatus.as_view(), name="customer-order-status"),
-]
